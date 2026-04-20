ea817cb5f3b60ed229cd2e032d2eab37a1115022
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2026-04-20 08:01:45 -1000

    chore: upgrade jsv dependency to 0.18.2
    Update the jsv library version in mix.exs and mix.lock to the latest patch
    release to include dependency fixes and improvements.

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule Sinter.MixProject do
       # Fast JSON parsing - the blazing fast gift library
       #      {:simdjsone, "~> 0.5.0"},
       # JSON Schema validation engine
-      {:jsv, "~> 0.18.1"},
+      {:jsv, "~> 0.18.2"},
       # Options validation
       {:nimble_options, "~> 1.1.1"},
       #      # Struct transformation library - for client app transformations
